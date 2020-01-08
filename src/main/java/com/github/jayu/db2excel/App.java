9d7cac31d24ab5e53e5333f20bd44fb080356118
jayu <402842327@qq.com>
2020-01-08 10:08:50 +0800

    初始化项目

---
+++ src/main/java/com/github/jayu/db2excel/App.java
@@ -0,0 +1,13 @@
+package com.github.jayu.db2excel;
+
+/**
+ * Created by Jay on 2020/1/8 10:04
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Jay on 2020/1/8 10:08
+ */
+
+public class AppTest {
+}
