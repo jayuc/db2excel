153eef70d0446a03966b5ded44209321403e956e
jayu <402842327@qq.com>
2020-01-08 11:17:01 +0800

    excel 工具类

--- src/main/java/com/github/jayu/db2excel/App.java
+++ src/main/java/com/github/jayu/db2excel/App.java
@@ -1,5 +1,16 @@
 package com.github.jayu.db2excel;
 
+import com.github.jayu.db2excel.com.github.jayu.db2excel.utils.ExcelUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Jay on 2020/1/8 10:04
  */
@@ -7,7 +18,22 @@ package com.github.jayu.db2excel;
 public class App {
 
     public static void main(String[] args) {
-        
+
+        final String path = "G:/log/a/f.xls";
+        List<Object> list = testData();
+
+        ExcelUtils.export(list, path);
+
+    }
+
+    static List<Object> testData(){
+        List<Object> list = new ArrayList<Object>();
+        Map<String, Object> row = new HashMap<>();
+        row.put("姓名", "余杰");
+        row.put("年龄", 23);
+        row.put("密码", "123456");
+        list.add(row);
+        return list;
     }
 
 }
---
+++ src/main/java/com/github/jayu/db2excel/com/github/jayu/db2excel/utils/ExcelUtils.java
@@ -0,0 +1,53 @@
+package com.github.jayu.db2excel.com.github.jayu.db2excel.utils;
+
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Jay on 2020/1/8 10:09
+ */
+
+public class ExcelUtils {
+
+    /**
+     * 导出 excel
+     * @param list    数据
+     * @param outputStream
+     * @param isXlsx  是否是 .xlsx 格式 ， 默认 .xls
+     */
+    public static void export(List<Object> list, OutputStream outputStream, boolean isXlsx){
+        ExcelWriter excelWriter = ExcelUtil.getWriter(isXlsx);
+        excelWriter.write(list, true);
+        excelWriter.flush(outputStream, true);
+        excelWriter.close();
+    }
+
+    /**
+     * 导出 excel
+     * @param list
+     * @param filePath 文件名
+     */
+    public static void export(List<Object> list, String filePath){
+        try {
+            OutputStream outputStream = new FileOutputStream(new File(filePath));
+            export(list, outputStream, isXlsx(filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isXlsx(String filePath){
+        if(filePath.endsWith(".xlsx")){
+            return true;
+        }
+        return false;
+    }
+
+}
